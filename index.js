22edcac2b7e041e773e7e64919ec81aba56a7620
vikram singh koshyari <vikramkoshyari222@gmail.com>
2021-02-13 18:53:44 +0530

    main folder build

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path =require('path')
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, './relationship-app/build')))
+app.use(express.static(path.join(__dirname, '/build')))
 
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, './relationship-app/build','index.html'));
+    res.sendFile(path.join(__dirname, '/build','index.html'));
 })
 app.listen(port,()=>{
     console.log(`server up at ${port}`)
