d7b091211e05aaed2203c488248cf1d2a4a96b85
vikram singh koshyari <vikramkoshyari222@gmail.com>
2021-02-13 18:45:59 +0530

    port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path =require('path')
 const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, './relationship-app/build')))
 
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, './relationship-app/build','index.html'));
 })
-app.listen(9000,()=>{
-    console.log('server up at 9000')
+app.listen(port,()=>{
+    console.log(`server up at ${port}`)
 })
